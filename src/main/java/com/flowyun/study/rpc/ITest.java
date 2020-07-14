6af98034b73a408a5923eb414d553f9413ff27f9
blue <wqy_blue3316@126.com>
2020-07-14 23:44:53 +0800

    feature(初始化): 初始化

---
+++ src/main/java/com/flowyun/study/rpc/ITest.java
@@ -0,0 +1,7 @@
+package com.flowyun.study.rpc;
+
+public interface ITest {
+
+    void study();
+
+}
---
+++ src/main/java/com/flowyun/study/rpc/TestService.java
@@ -0,0 +1,11 @@
+package com.flowyun.study.rpc;
+
+import org.apache.dubbo.config.annotation.Service;
+
+@Service
+public class TestService implements ITest{
+    @Override
+    public void study() {
+
+    }
+}
